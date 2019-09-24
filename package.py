b8c2b93318169d1b7c2ad714a5cae43623dc0a3f
Ahmed Siddique <siddiqua@greenitglobe.com>
2019-09-24 16:42:12 +0400

    Create package.py

---
+++ package.py
@@ -0,0 +1,53 @@
+from Jumpscale import j
+
+class Package(j.baseclasses.threebot_package):
+    """
+    to start need to run 
+    kosmos -p "j.tools.threebot_packages.get('www_inid',giturl='https://github.com/threefoldtech/www_inid.io.git',branch='development')"
+    kosmos -p "j.servers.threebot.default.start(web=True, ssl=False)"
+    """
+    def _init(self, **kwargs):
+        self.branch = kwargs["package"].branch or "master"
+        self.www_inid = "https://github.com/threefoldtech/www_inid.io.git"
+
+    def prepare(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        server = self.openresty
+        server.install(reset=True)
+        server.configure()
+        website = server.websites.get("www_inid")
+        website.ssl = False
+        website.port = 80
+        locations = website.locations.get("www_inid")
+        static_location = locations.locations_static.new()
+        static_location.name = "static"
+        static_location.path_url = "/"
+        path = j.clients.git.getContentPathFromURLorPath(self.www_inid, branch=self.branch, pull=True)
+        static_location.path_location = path
+        static_location.use_jumpscale_weblibs = True
+        website.path = path
+        locations.configure()
+        website.configure()
+
+    def start(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        self.prepare()
+    def stop(self):
+        """
+        called when the 3bot stops
+        :return:
+        """
+        pass
+
+    def uninstall(self):
+        """
+        called when the package is no longer needed and will be removed from the threebot
+        :return:
+        """
+        pas
